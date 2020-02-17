c18a5b87bc914d5eec4e99f9b47ed21d9ca8da44
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-17 13:07:43 -0800

    mapping thrugh images is setup.

--- 3-image-loopup/src/Components/ImageList.js
+++ 3-image-loopup/src/Components/ImageList.js
@@ -1,12 +1,39 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    console.log(props.images);
-    
-    return <div>
-        <h3>Image List</h3>
+    // console.log(props.images);
+    // const images = props.images.map((image) => {
+    //     return <img key = {image.id} src = {image.urls.regular} alt={image.description}/>
+    // });
 
+    const images = props.images.map(({ description, id, urls  }) => {
+        return <img key = {id} src = {urls.regular} alt={description}/>
+    });
+
+    return <div>
+        {images}
     </div>
 };
 
 export default ImageList;
+
+
+
+/*
+maps
+
+const maps = [ 2,1,3,4,5,6];
+let newNumbers = []
+for (let i = 0; i<numbers.length; i++){
+    newNumbers.push(numbers[i] * 10);
+}
+newNumbers;
+
+--------------
+
+numbers.map((num) => {
+    return num * 10;
+})
+console.log(numbers);
+
+*/
