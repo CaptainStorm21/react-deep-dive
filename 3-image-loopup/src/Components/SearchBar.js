b0d7e3562f6c8274ea1ffce449b53309b9e88755
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-16 21:45:09 -0800

    event change onSubmit is fixed

--- 3-image-loopup/src/Components/SearchBar.js
+++ 3-image-loopup/src/Components/SearchBar.js
@@ -5,18 +5,28 @@ class SearchBar extends Component {
   state = { term: "" };
   // default value    state = { term: "Time to find a new image!" };
 
-  onFormSubmit(event) {
-    //prevents automatic submission on enter and stops component from refreshing
+  // onFormSubmit(event) {
+  //   //One of the special features of arrow functions is that they automatically bind the value of this for
+  //   //all the code inside the function.
+  //   event.preventDefault();
+  //   console.log(this.state.term);
+  // }
+
+  // //arrow function
+  // onFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log(this.state.term);
+  // }
+  
+  onFormSubmit(event){
     event.preventDefault();
-    // error something like I cannot access the property state on the value undefined.
-    //console.log(this.state.term);
     console.log(this.state.term);
   }
 
   render() {
     return (
       <div className="ui  segment ">
-        <form onSubmit={this.onFormSubmit} className="ui form">
+        <form onSubmit={event => this.onFormSubmit(event)} className="ui form">
           <div className=" field">
             <label>Find an Image! </label>
             <input
@@ -50,3 +60,28 @@ export default SearchBar;
 // So pretty clear anytime that we type into the input on input change the method we just defined is going
 // to be called automatically and we will be given whatever which is typed into that input.
 //  console.log(event.target.value);
+
+/*
+  onFormSubmit(event) {
+    //prevents automatic submission on enter and stops component from refreshing
+    event.preventDefault();
+    // error something like I cannot access the property state on the value undefined.
+    //console.log(this.state.term);
+    console.log(this.state.term);
+  }
+
+*/
+
+/*
+solution
+
+onFormSubmit = event => {
+    event.preventDefault ();
+    console.log(this.state.term);
+}
+
+<form onSubmit = { this.onFormSubmit} className = "ui form">
+  <input type = "text" value = {this.state.term} onChange = { e => { this.setState ({ term: e.target.value})}} />
+</form>
+
+*/
