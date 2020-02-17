df42945bdc6f420b45f79a13908f8d39d0199cac
Captain Storm <CaptainStorm21@users.noreply.github.com>
2020-02-17 02:21:35 -0800

    picks up images number  from app.js

--- 3-image-loopup/src/Components/App.js
+++ 3-image-loopup/src/Components/App.js
@@ -6,21 +6,34 @@ import SearchBar from  './SearchBar'
 
 //convert from function component into class component
 class App extends React.Component {
+
+//set the state
+  state = {
+    //if result is expected to be an array use empty array
+    images: []
+  };
+
   //create a callback function that will drive the data from a form from SearchBar back into parent
- async onSearchSubmit(term){
+ onSearchSubmit = async (term) => {
    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query : term},
       headers: {
         Authorization: 'Client-ID xF3boNWFEMwXSnLb7BIPVXU0kFdC8a51zQJBDMOouCY' 
       }
     });
-    console.log(response.data.results);
+  
+    //console.log(this);
+    this.setState({ images: response.data.results });
+
+    //lists 10 results in console.log
+    // console.log(response.data.results);
   }
 
   render() {
     return (
       <div className = "margin-center container">
         <SearchBar onSubmit = {this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     )
   }
